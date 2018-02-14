9fcdf086982a882e8e73c343d17f1f8924681a56
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-14 23:50:11 +1100

    flipping smoother

--- public/assets/bundle.js
+++ public/assets/bundle.js
@@ -29567,7 +29567,8 @@ class BoardStore extends __WEBPACK_IMPORTED_MODULE_0_events__["EventEmitter"] {
     // cells are stored in an array
     this.cells = [{ id: 88828, backSide: false }];
 
-    this.flipping = false;
+    this.autoFlipper = false;
+    this.secondAutoFlipper = false;
   }
 
   cellSpecs() {
@@ -29595,18 +29596,31 @@ class BoardStore extends __WEBPACK_IMPORTED_MODULE_0_events__["EventEmitter"] {
 
   toggleRandFlipping() {
     console.log('store received toggleflip command');
-    // starts a timeout which keeps choosing a random cell and telling it to flip OR stops it if it's already going
-    if (this.flipping) {
-      this.flipping = false;
+    // starts two staggered intervals which keep choosing random numbers of random cells and flipping them
+    // if the intervals are already running, it stops them both
+    if (this.autoFlipper) {
+      clearInterval(this.autoFlipper);
+      clearInterval(this.secondAutoFlipper);
     } else {
-      this.flipping = setInterval(this.flipRandomCell.bind(this), 500);
+      this.autoFlipper = setInterval(this.flipSomeCells.bind(this), 2800);
+      this.secondAutoFlipper = setInterval(this.flipSomeCells.bind(this), 2100);
+    }
+  }
+
+  flipSomeCells() {
+    // chooses a number between 1 and 1/3 of the current max and executes that manay random flips
+    var times = Math.floor(Math.random() * (this.cells.length / 3)) + 1;
+    for (; times > 0; times--) {
+      this.flipRandomCell.call(this);
     }
   }
 
   flipRandomCell() {
-    console.log('flipping...' + this);
+    // chooses a cell at random from all the stored cells and changes it's clip orientation, emmitting a change event
     var cell = this.cells[Math.floor(Math.random() * this.cells.length)];
-    this.flipCell(cell.id);
+    cell.backSide = !cell.backSide;
+
+    this.emit('change');
   }
 
   addCell() {
