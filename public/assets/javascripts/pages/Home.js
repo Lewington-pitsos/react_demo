fb5119347a6df869e1f8a610d5c86794067929f0
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-15 23:13:20 +1100

    central scaffold ok

--- public/assets/javascripts/pages/Home.js
+++ public/assets/javascripts/pages/Home.js
@@ -3,7 +3,17 @@ import React from 'react'
 export default class Home extends React.Component {
   render() {
     return(
-      <h1>HomePage</h1>
+      <div>
+        <h1>HomePage</h1>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-6 text-center animated fadeIn">
+              <h3>Hey Ho</h3>
+              <p>The Ninja Ipsum Bot allows you to add blocks of Lorum Ipsum text of any length to your website quickly and easily. When designing a website, you often need to fill areas of the site in with dummy text to see how it will look with real content. The most common way of filling in this information is via a repeated stream of Latin text known as Lorum Ipsum. Using an online generator can be risky as they can contain hidden words or phrases, advertisements or bad language. Using a personal generator allows to be sure the text you are using is safe. The Ninja Ipsum Bot allows you to add blocks of Lorum Ipsum text of any length to your website quickly and easily. When designing a website, you often need to fill areas of the site in with dummy text to see how it will look with real content. The most common way of filling in this information is via a repeated stream of Latin text known as Lorum Ipsum. Using an online generator can be risky as they can contain hidden words or phrases, advertisements or bad language. Using a personal generator allows to be sure the text you are using is safe.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
