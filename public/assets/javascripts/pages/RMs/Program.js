20e39c640dd98d87d4890f023a500b1c8ad4ff65
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-16 20:06:47 +1100

    visual toichup

--- public/assets/javascripts/pages/RMs/Program.js
+++ public/assets/javascripts/pages/RMs/Program.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class Executor extends React.Component {
   render() {
     return(
-      <div className="col-md-4">
+      <div className="col-md-4 commands">
         <ol>
           <li>
             <p>Program</p>
