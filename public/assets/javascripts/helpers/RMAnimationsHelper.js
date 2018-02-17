2a232577134052785a90ec93800d1ed836d5e94e
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-17 13:26:41 +1100

    stone animation cleanup

--- public/assets/javascripts/helpers/RMAnimationsHelper.js
+++ public/assets/javascripts/helpers/RMAnimationsHelper.js
@@ -14,24 +14,31 @@ const RMAnimationsHelper =  {
   },
 
   uggAddStone() {
+    // animates the stone from ugg's sack to the stone icon in the adjacent bucket
+    // immidiately retusn the stone to it's innitla position
     var $stone = $('.uggs-stone')
-    var uggWidth = $('.ugg').width()
-    var bucketDistance = ($('#bucket-1').width() / 2) + 5
     $stone.animate({
-      right: uggWidth + bucketDistance
+      right: this.stoneDistance()
     }, 500, function() {
       $stone.css('right', 0)
     })
   },
 
   uggTakeStone() {
+    // teleports the stone from ugg's sack into behind the stone icon in the adjacent bucket
+    // animates the stone from the icon back to the sack
     var $stone = $('.uggs-stone')
-    var uggWidth = $('.ugg').width()
-    var bucketDistance = ($('#bucket-1').width() / 2) + 5
-    $stone.css('right', uggWidth + bucketDistance)
+    $stone.css('right', this.stoneDistance())
     $stone.animate({
       right: 0
     }, 500)
+  },
+
+  stoneDistance() {
+    // returns the distance the stone will have to travel from ugg's sack to the stone icon in the bucket
+    var uggWidth = $('.ugg').width()
+    var bucketDistance = ($('#bucket-1').width() / 2) + 5
+    return uggWidth + bucketDistance
   }
 }
 
