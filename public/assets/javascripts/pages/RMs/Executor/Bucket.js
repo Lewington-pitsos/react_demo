19d8fa785910a90b0e64c42b51103d7fa8d05fc6
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-17 12:41:09 +1100

    bucket visuals changes

--- public/assets/javascripts/pages/RMs/Executor/Bucket.js
+++ public/assets/javascripts/pages/RMs/Executor/Bucket.js
@@ -3,26 +3,15 @@ import React from 'react'
 import Stone from './Stone'
 
 export default class Bucket extends React.Component {
-
-  stonesArray(number) {
-    // returns an array of number Stone compoennets
-    var stones = []
-    for (var i = 0; i < number; i++) {
-      stones.push(<Stone key={i}/>)
-    }
-
-    return stones
-  }
-
   render() {
     // renders a number of stones according to props
-    var stones = this.stonesArray(this.props.stoneNumber)
 
     var animationClasses = this.props.newBucket ? 'animated fadeInUp' : ''
 
     return(
-      <div className={'bucket d-flex justify-content-around align-items-start flex-wrap ' + animationClasses} id={'bucket-' + this.props.id}>
-        {stones}
+      <div className={'bucket d-flex justify-content-center align-items-center ' + animationClasses} id={'bucket-' + this.props.id}>
+        <h2>{this.props.stoneNumber}x</h2>
+        <Stone />
       </div>
     )
   }
