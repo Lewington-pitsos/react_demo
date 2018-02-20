1b501aede366c2281401fb80cd8d0e6bf83bba42
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-20 20:36:50 +1100

    overlay over state panel while executing

--- public/assets/javascripts/pages/RM.js
+++ public/assets/javascripts/pages/RM.js
@@ -6,8 +6,20 @@ import ControlPanel from './shared/ControlPanel'
 import rmActions from '../actions/rmActions'
 import BucketSelector from './RM/BucketSelector'
 import ExecutePanel from './RM/ExecutePanel'
+import executionStore from '../stores/ExecutionStore'
 
 export default class RM extends React.Component {
+  constructor() {
+    super()
+    this.state = executionStore.getInfo()
+  }
+
+  componentWillMount() { // triggered just before a render occurs apparently
+    executionStore.on('change', () => {
+      this.setState( executionStore.getInfo() )
+    })
+  }
+
   addBucket() {
     rmActions.addBucket()
   }
@@ -25,6 +37,9 @@ export default class RM extends React.Component {
   }
 
   render() {
+
+    const hidden = this.state.executing ? '' : 'hidden'
+
     return(
       <div id="RMs-page">
         <div className="row justify-content-around register-machine m-0">
@@ -34,10 +49,12 @@ export default class RM extends React.Component {
           </div>
         </div>
         <ControlPanel classes="animated row" fadeIn="fadeInUp" fadeOut="fadeOutDown" GOLActive={true}>
-            <div className="col-md-6 p-0 edit d-flex justify-content-around flex-wrap">
+            <div className="col-md-6 p-0 edit d-flex justify-content-around flex-wrap position-relative">
               <button className="btn btn-primary" onClick={this.addBucket.bind(this)}>Add Bucket</button>
               <button className="btn btn-primary" onClick={this.removeBucket.bind(this)}>Add Bucket</button>
               <button className="btn btn-primary" onClick={this.addIncrement.bind(this)}>Add Command</button>
+              <div className={'overlay dark ' + hidden}>
+              </div>
             </div>
             <div className="col-md-6 p-0 execute">
               <ExecutePanel />
