54a5b67abf23f12f778c4bbd9049ad1c42c1492c
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-02-25 13:27:02 +1100

    randomizing process designed

--- public/assets/bundle.js
+++ public/assets/bundle.js
@@ -32908,22 +32908,37 @@ const sizzleDefaults = {
   zIndex: -1 // super conveniant
 };
 
-const sizzleDefaults2 = {
-  duration: 5000,
-  y: { 200: -200 }, // the start and end x and y axies for the motion
-  x: { 300: 300 },
-  easing: 'sin.out',
-  zIndex: -1 // super conveniant
+function generateSizzle(defaults) {
+  // generates and returns a slightly randomized version of the passed in object
 
+  return {
+    duration: randomize(defaults.duration),
+    y: { 200: -200 }, // the start and end x and y axies for the motion
+    x: { 400: 400 },
+    easing: 'sin.out',
+    zIndex: -1 // super conveniant
+  };
+}
 
-  // a duplucate object of the default paramaters object
-};const copy = Object.assign({}, sizzleDefaults);
+function randomize(number, deviation = false) {
+  // returns number with a random amount added or removed up to deviation
+  // if no deviation is supplied we default to 20% of the passed in number (rounded down)
+  deviation = deviation || number / 5;
+  // we add number to a randomized portion of deviation, whichc ould be positive or negative
+  return number + Math.random() * deviation * plusOrMinus();
+}
+
+function plusOrMinus() {
+  // returns 1 or -1
+  return Math.round(Math.random()) * 2 - 1;
+}
+
+const copy = Object.assign({}, sizzleDefaults);
 
 // a mojs Shapewirl created by passing in the copy of the sizzle animation parameters.
 const sizzle = new __WEBPACK_IMPORTED_MODULE_0_mo_js___default.a.ShapeSwirl(copy);
 
-const copy2 = Object.assign({}, sizzleDefaults2);
-const sizzle2 = new __WEBPACK_IMPORTED_MODULE_0_mo_js___default.a.ShapeSwirl(copy2);
+const sizzle2 = new __WEBPACK_IMPORTED_MODULE_0_mo_js___default.a.ShapeSwirl(generateSizzle(sizzleDefaults));
 
 const timeline = new __WEBPACK_IMPORTED_MODULE_0_mo_js___default.a.Timeline({
   repeat: 999
--- public/assets/javascripts/animations/background.js
+++ public/assets/javascripts/animations/background.js
@@ -3,28 +3,44 @@ import mojs from 'mo-js'
 // default paramaters for a sizzle animation
 const sizzleDefaults = {
   duration: 5000,
+  y: {200: -200}, // the start and end x and y axies for the motion
+  x: {400: 400},
+  easing: 'sin.out',
+  zIndex: -1 // super conveniant
+}
+
+
+function generateSizzle(defaults) {
+  // generates and returns a slightly randomized version of the passed in object
+
+  return {
+    duration: randomize(defaults.duration),
     y: {200: -200}, // the start and end x and y axies for the motion
     x: {400: 400},
     easing: 'sin.out',
     zIndex: -1 // super conveniant
+  }
 }
 
-const sizzleDefaults2 = {
-  duration: 5000,
-    y: {200: -200}, // the start and end x and y axies for the motion
-    x: {300: 300},
-    easing: 'sin.out',
-    zIndex: -1 // super conveniant
+function randomize(number, deviation=false) {
+  // returns number with a random amount added or removed up to deviation
+  // if no deviation is supplied we default to 20% of the passed in number (rounded down)
+  deviation = deviation || number / 5
+  // we add number to a randomized portion of deviation, whichc ould be positive or negative
+  return number + ((Math.random() * deviation) * plusOrMinus())
 }
 
-// a duplucate object of the default paramaters object
+function plusOrMinus() {
+  // returns 1 or -1
+  return Math.round(Math.random()) * 2 - 1
+}
+
 const copy = Object.assign({}, sizzleDefaults)
 
 // a mojs Shapewirl created by passing in the copy of the sizzle animation parameters.
 const sizzle = new mojs.ShapeSwirl(copy)
 
-const copy2 = Object.assign({}, sizzleDefaults2)
-const sizzle2 = new mojs.ShapeSwirl(copy2)
+const sizzle2 = new mojs.ShapeSwirl(generateSizzle(sizzleDefaults))
 
 const timeline = new mojs.Timeline({
   repeat: 999
